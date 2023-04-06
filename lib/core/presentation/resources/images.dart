823de70454e1678cddeb5744a9c417daecfc37f8
Héctor Alvarez <halvarezr@outlook.com>
2023-04-06 14:35:41 -0600

    improve images

--- lib/core/presentation/resources/images.dart
+++ lib/core/presentation/resources/images.dart
@@ -1,4 +1,9 @@
 abstract class Images {
   static const String _basePath = 'assets/images/';
-  static const String starWarsLogo = '${_basePath}star-wars-logo.png';
+  static const String starWarsLogo = '${_basePath}star-wars-solid-logo.png';
+  static const String starWarsBackground =
+      '${_basePath}star-wars-background.png';
+  static const String starsBackground = '${_basePath}stars-bg.jpg';
+  static const String deathStar = '${_basePath}death-star.png';
+  static const String millenniumFalcon = '${_basePath}millennium-falcon.png';
 }
