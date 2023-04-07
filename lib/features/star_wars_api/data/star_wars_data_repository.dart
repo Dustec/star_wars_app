b0c781417cd07cdf717ff5f109a0f91c06f20992
Héctor Alvarez <halvarezr@outlook.com>
2023-04-06 19:26:57 -0600

    Update star_wars_data_repository.dart

--- lib/features/star_wars_api/data/star_wars_data_repository.dart
+++ lib/features/star_wars_api/data/star_wars_data_repository.dart
@@ -1,13 +1,13 @@
 import '../../../core/domain/contracts/mapper.dart';
 import '../../../core/domain/managers/http_manager.dart';
-import '../../../core/remote/api.dart';
 import '../domain/models/star_wars_charater.dart';
 import '../domain/star_wars_repository.dart';
+import '../remote/star_wars_api.dart';
 import 'mapper/star_wars_character_from_dto.dart';
 import 'model/star_wars_character_dto.dart';
 import 'model/star_wars_get_people_response.dart';
 
-class StarWarsDataRepository with Api implements StarWarsRepository {
+class StarWarsDataRepository with StarWarsApi implements StarWarsRepository {
   StarWarsDataRepository({
     this.characterMapper = const StarWarsCharacterFromDtoMapper(),
   });
@@ -18,7 +18,7 @@ class StarWarsDataRepository with Api implements StarWarsRepository {
   Stream<List<StarWarsCharacter>> getPeople() async* {
     yield* http
         .get(
-          Uri.https(baseUrl, 'people/').toString(),
+          endpointParse('people/'),
         )
         .handle(
           mapper: (dynamic json) => StarWarsGetPeopleResponseDto.fromJson(json),
